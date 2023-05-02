9eabc712afba9a46b25be18f86b065f864cb63af
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-05-02 19:51:13 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH", "")
-    DB_URL = os.environ.get("DATABASE_URL", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5442493323:AAHPw8TNe0hh2zCAQKm_2O2o6KdmQ3Okgf8")
+    API_ID = int(os.environ.get("API_ID", 6534707))
+    API_HASH = os.environ.get("API_HASH", "4bcc61d959a9f403b2f20149cbbe627a")
+    DB_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Uploader:Uploader@cluster0.ba0ppxa.mongodb.net/?retryWrites=true&w=majority")
